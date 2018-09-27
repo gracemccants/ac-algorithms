4e8397ea24e25c6331aef304adb2f8bc1ee15c4f
Grace McCants <grace.mccants@gmail.com>
2018-09-27 10:44:20 -0700

    completed cyclicRotation exercise

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -14,6 +14,20 @@
 //
 //    For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].\
 
+//input: an array of numbers and a number
+//output: rotated array
+//purpose: to take in an array and shift its elements to the right by one position for the given input number
+
 function cyclicRotation(arr, rotation){
-    //Your code here...
+    //use a for loop to make it happen a number of times K
+    for (var i = 0; i > rotation; i++) {
+        //use .pop to grab the last element in the array and save the output of the .pop into a variable
+        var lastNumber = arr.pop();
+        //unshift (add to the beginning) the variable to the beginning of the array
+        arr.unshift(lastNumber);
+    }
+    //return the array
+    return arr;
 }
+var result = cyclicRotation([3, 8, 9, 7, 6], 1);
+console.log(result);
