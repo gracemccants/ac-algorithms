d5b92996e5d335a9b6ee5af26d6a24105fc1af48
Grace McCants <grace.mccants@gmail.com>
2018-10-15 10:55:38 -0700

    completed dnaPairing exercise

--- dnaPairing.js
+++ dnaPairing.js
@@ -10,11 +10,38 @@
 
 //The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 
-function pairElement(str) {
-  //your code here
-}
+//input: string
+//output: an array
+//purpose: to match each element with its base pair AT & CG and return an array of base pairs.
+//create an empty array that holds the pairArray
+//split the string into characters
+//create a for loop that loops through the result of the split (is it an array?)
+  //conditional statement that checks to see if it strictly equal to any of the possible letters and then adds the appropriate corresponding letter
+  //add it to pairArray by pushing it
 
-pairElement("GCG");
+  function pairElement(str) {
+    var pairArray =[];
+    var splitString = str.split("");
+    for (var i = 0; i < splitString.length; i++){
+      if (splitString[i] === "G"){
+        var basePair = ["G","C"];
+        pairArray.push(basePair);
+      } else if (splitString[i] === "C"){
+        var basePair = ["C","G"];
+        pairArray.push(basePair);
+      } else if (splitString[i] === "A"){
+        var basePair = ["A","T"];
+        pairArray.push(basePair);
+    } else {
+      var basePair = ["T","A"];
+      pairArray.push(basePair);
+    }
+  }
+    return pairArray;
+  };
+  
+  var results = pairElement("GCG");
+  console.log(results);
 
 //TEST CASES
 //pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
